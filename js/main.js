af453545ca366133fbc8d0343746c78d554c1c51
R123432 <boxredouane11@gmail.com>
2026-02-14 04:05:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,32 @@ const container = document.getElementById("products");
 
 if(container){
 products.forEach(product=>{
+
+let finalPrice = product.price - (product.price * product.discount / 100);
+
 container.innerHTML += `
-<div class="card">
+<div class="card fade-in">
 <img src="${product.image}">
 <div class="card-content">
 <h3>${product.name}</h3>
-<p class="price">${product.price} DH</p>
-<a class="btn" onclick="addToCart(${product.id})">أضف للسلة</a>
+
+<p class="price">
+${finalPrice} DH 
+<span style="text-decoration:line-through;color:gray;">
+${product.price}
+</span>
+</p>
+
+<div class="badge">خصم ${product.discount}%</div>
+
+<div class="rating">⭐⭐⭐⭐☆</div>
+
+<p style="color:gold;">باقي ${product.stock} فقط 🔥</p>
+
+<a class="btn" onclick="addToCart(${product.id})">
+أضف للسلة
+</a>
+
 </div>
 </div>
 `;
