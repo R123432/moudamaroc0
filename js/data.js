5f4904dcca45fd0c019e1a7a1d80cb85f48fb49c
R123432 <boxredouane11@gmail.com>
2026-02-14 06:10:10 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,18 @@
-let products = JSON.parse(localStorage.getItem("products"));
-
-if(!products){
-products = [];
-localStorage.setItem("products", JSON.stringify(products));
+let products = JSON.parse(localStorage.getItem("products")) || [
+{
+id:1,
+name:"Nike Shoes",
+price:299,
+stock:10,
+discount:0,
+image:"https://via.placeholder.com/250"
+},
+{
+id:2,
+name:"Adidas Shirt",
+price:199,
+stock:15,
+discount:10,
+image:"https://via.placeholder.com/250"
 }
+];
