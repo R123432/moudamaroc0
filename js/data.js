0352af7ca584d9029aeb6f0c7c4d8b43052e5fbd
R123432 <boxredouane11@gmail.com>
2026-02-14 03:24:32 +0100

    Create js/data.js

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const products = [
+{
+id:1,
+name:"قميص فاخر 2025",
+price:300,
+image:"https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+sizes:["S","M","L","XL"],
+colors:["أسود","أبيض","أزرق"]
+},
+{
+id:2,
+name:"حذاء أنيق عالمي",
+price:500,
+image:"https://images.unsplash.com/photo-1542291026-7eec264c27ff",
+sizes:["40","41","42","43"],
+colors:["أسود","أبيض"]
+}
+];
